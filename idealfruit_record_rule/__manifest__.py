4ba8f00feb309a4de15a28634c5345c4662ac190
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-02-20 01:35:59 +0100

    new module

---
+++ idealfruit_record_rule/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Manuel Calero (https://xtendoo.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Reglas de registro de Idealfruit",
+    "version": "16.0.0.1",
+    "author": "Manuel Calero (https://xtendoo.es)",
+    "category": "Idealfruit",
+    "summary": "Reglas de registro de Idealfruit",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "product",
+        "idealfruit_vendor_checklist",
+    ],
+    "data": [
+        "security/idealfruit_security.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
